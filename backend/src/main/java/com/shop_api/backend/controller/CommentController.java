ec75b06b7c635dbd382ab0a9e9ee4a5226b3df3e
LeeTr4ng <lephamthuytrang1810@gmail.com>
2025-11-24 13:00:09 +0700

    refactor: remove unused import for RequestBody in CommentController

--- backend/src/main/java/com/shop_api/backend/controller/CommentController.java
+++ backend/src/main/java/com/shop_api/backend/controller/CommentController.java
@@ -7,13 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shop_api.backend.dto.CommentDto;
 import com.shop_api.backend.service.comment.CommentService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("${api.prefix}/comments")
